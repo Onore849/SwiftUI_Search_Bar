13d4c01a0d8348510a7c63fd8f6a65d4f6ce646e
Onore849 <rion3376@gmail.com>
2020-08-31 06:13:26 +0900

    Initial Commit

---
+++ SearchBar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SearchBar
+//
+//  Created by 野澤拓己 on 2020/08/31.
+//  Copyright © 2020 Takumi Nozawa. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
